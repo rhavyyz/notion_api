26108c3bd0ef5978d56666a5d45e33b6b315ff8e
Rhavy Frota <rhavyf1411@gmail.com>
2025-05-12 01:41:59 -0300

    Add like and comment models

--- project/api/models.py
+++ project/api/models.py
@@ -7,4 +7,50 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_datetime = models.DateTimeField(auto_now_add=True, blank=True)
-    
+   
+    @property
+    def like_qtd(self):
+        return Like.objects.filter(post__id=self.id).count()
+
+
+class Comment(models.Model):
+    username = models.CharField(max_length=100)
+    post = models.ForeignKey(
+                             Post, 
+                             on_delete=models.CASCADE, 
+                             null=True,
+                             blank = True
+                            )   
+    comment = models.ForeignKey(
+                             'self',
+                             related_name='+',
+                             on_delete=models.CASCADE, 
+                             null=True,
+                             blank = True
+                            )
+    content = models.TextField()
+    created_datetime = models.DateTimeField(auto_now_add=True, blank=True)
+ 
+    @property
+    def like_qtd(self):
+        return Like.objects.filter(comment__id=self.id).count()
+
+
+class Like(models.Model):
+    post = models.ForeignKey(
+                             Post, 
+                             on_delete=models.CASCADE, 
+                             null=True,
+                             blank = True
+                            )   
+    comment = models.ForeignKey(
+                             Comment, 
+                             on_delete=models.CASCADE, 
+                             null=True,
+                             blank = True
+                            )
+
+    username = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = [['post', 'username'], ['comment', 'username']]
